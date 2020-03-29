8c252c7957c4be2f5dec1f7e51577f4593b9581f
aw84 <silencewalker@gmail.com>
2020-03-29 19:16:25 +0200

    Update build_python.sh

--- build_python.sh
+++ build_python.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tar zxf /Python-3.7.4.tgz -C / && \
+tar zxf /data/Python-3.7.4.tgz -C / && \
 cd /Python-3.7.4 && \
 ./configure --prefix=/data/linux_build_3.7.4 && \
 make -j4 && make -j4 install
