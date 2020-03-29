5118a955ed2582f9187db5d61ae9771479b90d78
aw84 <silencewalker@gmail.com>
2020-03-29 19:15:41 +0200

    Volumes on windows are case insensitive

--- build_python.sh
+++ build_python.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tar zxf /data/Python-3.7.4.tgz -C /data/ && \
-cd /data/Python-3.7.4 && \
-./configure --prefix=/data/linux_py37 && \
+tar zxf /Python-3.7.4.tgz -C / && \
+cd /Python-3.7.4 && \
+./configure --prefix=/data/linux_build_3.7.4 && \
 make -j4 && make -j4 install
